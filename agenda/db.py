9124f40260930d6d30662a2b259e77e002992413
Emmanuel Ramirez <161186127+manolitobes@users.noreply.github.com>
2025-09-18 23:35:42 -0600

    postgres
    conexion con postgresql

--- agenda/db.py
+++ agenda/db.py
@@ -1,19 +1,12 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SQLITE = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME' : os.path.join(BASE_DIR, 'db/sqlite/db.sqlite3'),
-    }
-}
-
 POSTGRESQL = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
+        'NAME': 'agenda',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': 'emma2002',
         'HOST': 'localhost',
         'PORT': '5432',
 
