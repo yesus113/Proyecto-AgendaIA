562341061a5f1f57f66bcafda7973f4fac1f71bc
yesus113 <147835419+yesus113@users.noreply.github.com>
2025-09-25 23:35:28 -0600

    Model Task

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    state = models.CharField(
+        max_length=20,
+        choices=[
+            ("sin empezar", "Sin Empezar"),
+            ("en curso", "En Curso"),
+            ("listo", "Listo"),
+        ],
+        default="sin empezar")
+    
+    deadline = models.DateTimeField(null=True, blank=True)
+    created = models.DateTimeField(datetime.now())
+    fk_user_id = models.ForeignKey(  User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.title
+
+    
+    
